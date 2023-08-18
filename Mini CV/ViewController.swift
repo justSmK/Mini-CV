3677ea7f577dd3d57ad5e452e2113496f7d51ec3
justSmK <28645140+justSmK@users.noreply.github.com>
2023-08-18 20:01:55 +0300

    Initialization of the project

---
+++ Mini CV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini CV
+//
+//  Created by Sergei Semko on 8/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
