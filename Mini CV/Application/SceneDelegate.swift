68794a89f4f02145db6db4985d04a8a6fd93996a
Sergei Semko <28645140+justSmK@users.noreply.github.com>
2023-08-18 21:28:35 +0300

    Project preparation (#1)
    * Initialization of the project
    
    * Delete storyboard
    
    * Change Target's "Application scene manifest"
    
    * Setup SceneDelegate
    
    * Setup directories

---
+++ Mini CV/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  Mini CV
+//
+//  Created by Sergei Semko on 8/18/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ Mini CV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini CV
+//
+//  Created by Sergei Semko on 8/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
