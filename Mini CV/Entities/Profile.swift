5bd95000fd714fd2e9a727f3521713e41d058c32
justSmK <28645140+justSmK@users.noreply.github.com>
2023-08-20 17:01:16 +0300

    Preparing for localization

---
+++ Mini CV/Entities/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  Mini CV
+//
+//  Created by Sergei Semko on 8/20/23.
+//
+
+struct Profile: Codable {
+    let imageName: String
+    let fullName: String
+    let description: String
+    let location: String
+    let about: String
+}
